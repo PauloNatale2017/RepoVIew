f8bc918f04adfdc883df1ff8b185d0867f1d7c3d
GRUPOLINCES\paulo.natale <paulo.natale@dekra.com>
2019-05-30 15:08:19 -0300

    Atualização DinamicView

--- DINAMICVIEW.WEB/DINAMICVIEW.WEB/js/Login_v5.js
+++ DINAMICVIEW.WEB/DINAMICVIEW.WEB/js/Login_v5.js
@@ -1,52 +1,21 @@
+//var app = angular.module('App', ['blockUI']);
+//var Objcs;
+//var token = "";
 
-(function ($) {
-    "use strict";
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
+//app.controller("CtrlLogin_v5", ['$scope', '$http', '$location', '$window', 'blockUI', '$timeout',
+//function ($scope, $http, $location, $window, blockUI, $timeout) {
 
-    $('.validate-form').on('submit',function(){
-        var check = true;
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
-        return check;
-    });
+//}]);
 
+function Submmit() {
 
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
+    var entity = {
+        UserName: document.getElementById("first-name").value,
+        Password: document.getElementById("input100").value
     }
+}
 
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
 
-        $(thisAlert).addClass('alert-validate');
-    }
 
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
 
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
 
-})(jQuery);
